b78f1f52ecb2367a612cc0cc7bb674a624d9510b
zabrams <zachary.abrams@gmail.com>
2015-05-22 14:39:40 -0700

    changing to hola

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def hello
-  	render text: "hello world!"
+  	render text: "hola mundo!"
   end
 
   protect_from_forgery with: :exception
